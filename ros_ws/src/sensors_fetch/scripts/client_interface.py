ca5d79d21bfdff13723a831ade4520af6ae9ad41
Thiago Lages <thiagolagesrocha@gmail.com>
2019-05-21 21:25:57 +1000

    Add several changes on packages and code. Robot is fully functional on simple_task_world_coordinate.py

--- ros_ws/src/sensors_fetch/scripts/client_interface.py
+++ ros_ws/src/sensors_fetch/scripts/client_interface.py
@@ -1,32 +1,7 @@
 #!/usr/bin/env python
 
-# Copyright (c) 2015, Fetch Robotics Inc.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-#     * Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-#     * Neither the name of the Fetch Robotics Inc. nor the names of its
-#       contributors may be used to endorse or promote products derived from
-#       this software without specific prior written permission.
-# 
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL FETCH ROBOTICS INC. BE LIABLE FOR ANY
-# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-# Author: Michael Ferguson
+# Definition of classes that might be useful to the main program (simple task) 
+# when it comes to moving the base of the robot, grasping, etc
 
 import copy
 import actionlib
@@ -240,77 +215,3 @@ class GraspingClient(object):
             result = self.move_group.moveToJointPosition(joints, pose, 0.02)
             if result.error_code.val == MoveItErrorCodes.SUCCESS:
                 return
-
-if __name__ == "__main__":
-    # Create a node
-    rospy.init_node("demo")
-
-    # Make sure sim time is working
-    while not rospy.Time.now():
-        pass
-
-    # Setup clients
-    move_base = MoveBaseClient()
-    torso_action = FollowTrajectoryClient("torso_controller", ["torso_lift_joint"])
-    head_action = PointHeadClient()
-    grasping_client = GraspingClient()
-
-    # Move the base to be in front of the table
-    # Demonstrates the use of the navigation stack
-    rospy.loginfo("Welcome ! This is the simple_task script for the 'Sensors' subject.")
-
-    rospy.loginfo("Moving to table...")
-    move_base.goto(2.250, 3.118, 0.0)
-    move_base.goto(2.750, 3.118, 0.0)
-
-    # Raise the torso using just a controller
-    rospy.loginfo("Raising torso...")
-    torso_action.move_to([0.4, ])
-
-    # Point the head at the cube we want to pick
-    head_action.look_at(3.7, 3.18, 0.0, "map")
-
-    # Get block to pick
-    while not rospy.is_shutdown():
-        rospy.loginfo("Picking object...")
-        grasping_client.updateScene()
-        cube, grasps = grasping_client.getGraspableCube()
-        if cube == None:
-            rospy.logwarn("Perception failed.")
-            continue
-
-        # Pick the block
-        if grasping_client.pick(cube, grasps):
-            break
-        rospy.logwarn("Grasping failed.")
-
-    # Tuck the arm
-    grasping_client.tuck()
-
-    # Lower torso
-    rospy.loginfo("Lowering torso...")
-    torso_action.move_to([0.0, ])
-
-    # Move to second table
-    rospy.loginfo("Moving to second table...")
-    move_base.goto(-3.53, 3.75, 1.57)
-    move_base.goto(-3.53, 4.15, 1.57)
-
-    # Raise the torso using just a controller
-    rospy.loginfo("Raising torso...")
-    torso_action.move_to([0.4, ])
-
-    # Place the block
-    while not rospy.is_shutdown():
-        rospy.loginfo("Placing object...")
-        pose = PoseStamped()
-        pose.pose = cube.primitive_poses[0]
-        pose.pose.position.z += 0.05
-        pose.header.frame_id = cube.header.frame_id
-        if grasping_client.place(cube, pose):
-            break
-        rospy.logwarn("Placing failed.")
-
-    # Tuck the arm, lower the torso
-    grasping_client.tuck()
-    torso_action.move_to([0.0, ])
---
+++ ros_ws/src/sensors_fetch/scripts/simple_task_world_coordinate.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+
+import rospy
+from moveit_msgs.msg import MoveItErrorCodes
+from moveit_python import MoveGroupInterface, PlanningSceneInterface
+from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
+
+import moveit_commander
+# class definition that is useful for moving base around (file inside our package)
+from client_interface import MoveBaseClient 
+
+def move_arm(gripper_poses):
+    
+    # This is the wrist link not the gripper itself
+    gripper_frame = 'wrist_roll_link'
+
+    # Construct a "pose_stamped" message as required by moveToPose
+    gripper_pose_stamped = PoseStamped()
+    gripper_pose_stamped.header.frame_id = 'base_link'
+
+    for pose in gripper_poses:
+        #rospy.loginfo("Inside 'pose' loop")
+        # Finish building the Pose_stamped message
+        gripper_pose_stamped.header.stamp = rospy.Time.now()
+        gripper_pose_stamped.pose = pose
+
+        rospy.loginfo("Now shaking arm")
+
+        # Move gripper frame to the pose specified
+        move_group.moveToPose(gripper_pose_stamped, gripper_frame)
+        result = move_group.get_move_action().get_result()
+
+        if check_result(result):
+            rospy.loginfo("Hello there!")
+
+def tuck_arm():
+    global move_group, tuck_joint_pos, joints
+    
+    rospy.loginfo("Now tucking arm")
+    move_group.moveToJointPosition(joints, tuck_joint_pos)
+    result = move_group.get_move_action().get_result()
+    if check_result(result):
+        rospy.loginfo("Tucking complete!")
+
+#def lower_torso():
+
+
+def define_ground_plane():
+    # Define ground plane
+    # This creates objects in the planning scene that mimic the ground
+    # If these were not in place gripper could hit the ground
+    planning_scene = PlanningSceneInterface("base_link")
+    planning_scene.removeCollisionObject("my_front_ground")
+    planning_scene.removeCollisionObject("my_back_ground")
+    planning_scene.removeCollisionObject("my_right_ground")
+    planning_scene.removeCollisionObject("my_left_ground")
+    planning_scene.addCube("my_front_ground", 2,  1.1,  0.0, -1.0)
+    planning_scene.addCube("my_back_ground" , 2, -1.2,  0.0, -1.0)
+    planning_scene.addCube("my_left_ground" , 2,  0.0,  1.2, -1.0)
+    planning_scene.addCube("my_right_ground", 2,  0.0, -1.2, -1.0)
+
+    planning_scene.addCube("my_front_plane" , 2,  0.5, 0.0, 1.0) # avoid hitting the table
+
+def check_result(result):
+    if result:
+        # Checking the MoveItErrorCode
+        if result.error_code.val == MoveItErrorCodes.SUCCESS:
+            return True
+        else:
+            # If you get to this point please search for:
+            # moveit_msgs/MoveItErrorCodes.msg
+            rospy.logerr("Arm goal in state: %s",
+            move_group.get_move_action().get_state())
+            return False
+    else:
+        rospy.logerr("MoveIt! failure no result returned.")
+
+
+# Note: fetch_moveit_config move_group.launch must be running
+# Safety!: Do NOT run this script near people or objects.
+# Safety!: There is NO perception.
+#          The ONLY objects the collision detection software is aware
+#          of are itself & the floor.
+if __name__ == '__main__':
+
+    # More info
+    # http://docs.ros.org/kinetic/api/moveit_tutorials/html/doc/move_group_python_interface/move_group_python_interface_tutorial.html
+    
+    rospy.init_node("simple_task_world_coordinate")
+    
+    print("HEEEEELLO")
+    
+    define_ground_plane()
+    
+    # Create move group interface for a fetch robot
+    move_group = MoveGroupInterface("arm_with_torso", "base_link")
+    move_base  = MoveBaseClient()
+
+    # If we want to move the robot around
+    #position = [2.250, 3.118,  1.57]
+    #position = [4.050, 4.050, -1.57]
+    #rospy.loginfo("Moving to a different position")
+    #move_base.goto(position[0], position[1], position[2])
+    
+    # Position and rotation of two "wave end poses"
+    p1 = Pose( Point(0.042, 0.384, 1.826), Quaternion(0.173, -0.693, -0.242, 0.657) )
+    p2 = Pose( Point(0.047, 0.545, 1.822), Quaternion(-0.274, -0.701, 0.173, 0.635) )
+    gripper_poses = [p1, p2]
+
+    tuck_joint_pos = [1.32, 1.40, -0.2, 1.72, 0.0, 1.66, 0.0]
+    joints = ["shoulder_pan_joint", "shoulder_lift_joint", "upperarm_roll_joint",
+                  "elbow_flex_joint", "forearm_roll_joint", "wrist_flex_joint", "wrist_roll_joint"]
+    robot = moveit_commander.RobotCommander()
+    group = moveit_commander.MoveGroupCommander("arm")
+
+    # To get the robot full curent state:
+    #current_state = robot.get_current_state()
+    #print ("============ Printing robot state")
+    #print(current_state)
+    #print ("============")
+
+    # To get just joint angles:
+    group_variable_values = group.get_current_joint_values()
+    print "============ Joint values: ", group_variable_values
+
+
+    while not rospy.is_shutdown():
+	        
+        move_arm(gripper_poses)
+        tuck_arm()
+        rospy.sleep(3)
+
+	        
+
+    # This stops all arm movement goals
+    # It should be called when a program is exiting so movement stops
+    move_group.get_move_action().cancel_all_goals()
